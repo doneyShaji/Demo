6f7c8313d51d165373cd7b031215ea6250aef276
doneyShaji <doney.shaji@people10.com>
2024-04-17 18:30:14 +0530

    Development of a few Basic Apps

---
+++ Alliegiant/Alliegiant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alliegiant
+//
+//  Created by P10 on 16/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ navigationController/navigationController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  navigationController
+//
+//  Created by P10 on 15/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
