9db47ec9d42202ec0d345291cda902ab3acb8853
doneyShaji <doney.shaji@people10.com>
2024-04-18 17:26:54 +0530

    UI Modified for Table View

--- TableView/Alliegiant/ViewController.swift
+++ TableView/Alliegiant/ViewController.swift
@@ -20,13 +20,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     let data: [Apple] = [
-        Apple(title: "MacBook Air M2", imageName: "Image1", productPrice: "₹ 99,900", deliveryDescprition: "FREE delivery Sat, 20 Apr", productDescription: "Brand: Apple\n Model Name: MacBook Air \n Screen Size:  13.6"),
-        Apple(title: "iPhone 15 Pro Max", imageName: "image2", productPrice: "₹ 1,48,800", deliveryDescprition: "FREE delivery Sat, 20 Apr", productDescription: "8GB"),
-        Apple(title: "Apple Watch SE", imageName: "image3", productPrice: "₹ 30,000", deliveryDescprition: "FREE delivery Sat, 20 Apr", productDescription: "8GB"),
-        Apple(title: "AirPods (3rd generation)", imageName: "image4", productPrice: "₹ 19,900", deliveryDescprition: "FREE delivery Sat, 20 Apr", productDescription: "8GB"),
-        Apple(title: "iMac", imageName: "image5", productPrice: "₹ 1,34,900", deliveryDescprition: "FREE delivery Sat, 20 Apr", productDescription: "8GB"),
-        Apple(title: "Apple TV", imageName: "image6", productPrice: "₹ 14,900", deliveryDescprition: "FREE delivery Sat, 20 Apr", productDescription: "8GB"),
-        Apple(title: "AirPods Max", imageName: "image7", productPrice: "₹ 59,900", deliveryDescprition: "FREE delivery Sat, 20 Apr", productDescription: "8GB")
+        Apple(title: "MacBook Air M2", imageName: "Image1", productPrice: "₹ 99,900", deliveryDescprition: "FREE delivery Sat, 20 Apr", productDescription: " Brand: Apple\n Model Name: MacBook Air \n Screen Size:  13.6\n Hard Disk Size: 256 GB\n RAM Memory: 8 GB"),
+        Apple(title: "iPhone 15 Pro Max", imageName: "image2", productPrice: "₹ 1,48,800", deliveryDescprition: "FREE delivery Sat, 20 Apr", productDescription: " Brand: Apple\n Model Name: iPhone 15 Pro Max \n Operating System: iOS\n Cellular Technology: 5G"),
+        Apple(title: "Apple Watch SE", imageName: "image3", productPrice: "₹ 30,000", deliveryDescprition: "FREE delivery Sat, 20 Apr", productDescription: " Brand: Apple\n Style: GPS\n Special Feature: Heart Rate Monitor\n Shape: Square"),
+        Apple(title: "AirPods (3rd generation)", imageName: "image4", productPrice: "₹ 19,900", deliveryDescprition: "FREE delivery Sat, 20 Apr", productDescription: " Brand: Apple\n Model Name: AirPods Pro (3rd Gen, 2024)\n Colour: White\n Form Factor: In Ear\n Connectivity Technology: Wireless"),
+        Apple(title: "iMac", imageName: "image5", productPrice: "₹ 1,34,900", deliveryDescprition: "FREE delivery Sat, 20 Apr", productDescription: " Brand: Apple\n Operating System: Mac OS\n Memory Storage Capacity: 256 GB\n Screen Size: 24 Inches\n RAM Memory: 8 GB\n CPU Model: Apple M2 Ultra"),
+        Apple(title: "Apple TV", imageName: "image6", productPrice: "₹ 14,900", deliveryDescprition: "FREE delivery Sat, 20 Apr", productDescription: " Brand: Apple\n Connectivity Technology: Bluetooth, Wi-Fi\n Connector Type: DMI\n Special Feature: High Definition\n Resolution: 4k\n Supported Services: Netflix, HBO Max"),
+        Apple(title: "AirPods Max", imageName: "image7", productPrice: "₹ 59,900", deliveryDescprition: "FREE delivery Sat, 20 Apr", productDescription: " BRAND: Apple\n MODEL NAME: Airpods Max\n HEADPHONE TYPE: Over the head\n WIRED/WIRELESS: Wireless")
         
     ]
     override func viewDidLoad() {
@@ -49,6 +49,8 @@ extension ViewController{
         detailVC.myString = data[indexPath.row].title
         detailVC.myDisplayImage = UIImage(named: data[indexPath.row].imageName)
         detailVC.descriptionLabel = data[indexPath.row].productDescription
+        detailVC.priceContent = data[indexPath.row].productPrice
+        detailVC.deliveryContent = data[indexPath.row].deliveryDescprition
         navigationController?.pushViewController(detailVC, animated: true)
     }
     
--- TableView/Alliegiant/mySecondViewController.swift
+++ TableView/Alliegiant/mySecondViewController.swift
@@ -10,20 +10,24 @@ import UIKit
 class mySecondViewController: UIViewController {
 
     @IBOutlet weak var myLabel: UILabel!
-    
     @IBOutlet weak var myImage: UIImageView!
     //@IBOutlet weak var myImage: UIImageView!
     @IBOutlet weak var descriptionSeconVC: UILabel!
+    @IBOutlet weak var priceDisplaySecondViewController: UILabel!
+    @IBOutlet weak var deliveryDisplay: UILabel!
     
     var myString: String?
     var myDisplayImage: UIImage?
     var descriptionLabel: String?
-    
+    var priceContent: String?
+    var deliveryContent: String?
     override func viewDidLoad() {
         super.viewDidLoad()
         myLabel.text = myString
         myImage.image = myDisplayImage
         descriptionSeconVC.text = descriptionLabel
+        priceDisplaySecondViewController.text = priceContent
+        deliveryDisplay.text = deliveryContent
         
         // Do any additional setup after loading the view.
     }
